c02983a3b6ff4694d80b289d3c92dac8b1917407
Haichen Li <lhaiche@amazon.com>
2022-12-01 16:31:24 -0800

    [gpt2] rewrite GPT2ForSampling using DecoderProgram
    GitOrigin-RevId: 75e7f20186e9e17b2f31c19357a09c1c16eff4ac

--- src/transformers_neuronx/gpt2/model.py
+++ src/transformers_neuronx/gpt2/model.py
@@ -14,111 +14,100 @@
 # ==============================================================================
 import torch
 from torch.nn.parameter import UninitializedParameter
+from transformers_neuronx import compiler
 from transformers_neuronx import dtypes
 from transformers_neuronx import module
+from transformers_neuronx import ops
 from transformers_neuronx import parallel
+from transformers_neuronx import program
+from transformers_neuronx import utils
 from transformers_neuronx.gpt2 import hlo
 from transformers_neuronx.gpt2.config import GPT2Config
 
 
 class GPT2ForSampling(module.PretrainedModel):
 
-    def __init__(self, config, batch_size=1, n_active_tokens=1, amp='f32', tp_degree=2,
-                 unroll=False, fast_init=False, **kwargs):
+    def __init__(self, config, batch_size=1, amp='f32', tp_degree=2,
+                 unroll=False, init_n_active_tokens=None, **kwargs):
         super().__init__()
         config = GPT2Config(config, batch_size, amp, tp_degree, **kwargs)
-        n_positions = config.n_positions
-        self.gpt2_kernel = None
-        if unroll:
-            block_kernel = None
-            ln_lm_head_kernel = None
-            self.gpt2_kernel = hlo.build_gpt2_kernel(config, n_active_tokens, n_positions)
-        else:
-            block_kernel = hlo.build_gpt2_block_kernel(config, n_active_tokens, n_positions)
-            ln_lm_head_kernel = hlo.build_lm_head_kernel(config, n_active_tokens)
-        self.transformer = GPT2Transformer(config, block_kernel)
-        dtype = dtypes.to_torch_dtype(config.amp)
-        self.lm_head = module.LowMemoryLazyLinear(config.vocab_size, dtype=dtype, bias=False)
         self.config = config
-        ln_f = self.transformer.ln_f
-        self.ln_lm_head = GPT2LnLmHead(config, ln_lm_head_kernel, None, ln_f, self.lm_head)
-        self.manipulator = parallel.TensorManipulator(config.tp_degree)
-        self.gpt2_caches = None
-        self.gpt2_params = None
+        if unroll is None:
+            unroll = config.n_layer
+        self.unroll = unroll
+        self.init_n_active_tokens = init_n_active_tokens
+        dtype = dtypes.to_torch_dtype(config.amp)
+        self.transformer = GPT2Transformer(config)
+        self.lm_head = module.LowMemoryLazyLinear(config.vocab_size, dtype=dtype, bias=False)
+        self.ln_lm_head = GPT2LnLmHead(config, self.transformer.ln_f, self.lm_head)
+        self.n_positions_list = utils.power_of_two_bucket_sizes(128, config.n_positions)
+        self.program = None
+        self.init_program = program.DoNothingDecoder()
+        self.manipulator = parallel.ParallelTensorManipulator(config.tp_degree)
+        self.to_neuron_hooks = []
 
     def to_neuron(self):
+        ops.init()
+        config = self.config
+        n_positions_list = self.n_positions_list
+        unroll = self.unroll
+        self.program = build_gpt2_program(config, 1, n_positions_list, unroll)
+        init_n_active = self.init_n_active_tokens
+        if init_n_active is not None:
+            self.init_program = build_gpt2_program(config, init_n_active, n_positions_list, unroll)
         self.transformer.wte.materialize()
         self.transformer.wpe.materialize()
-        if self.gpt2_kernel is not None:
-            self.gpt2_kernel.load()
         for idx, block in enumerate(self.transformer.h):
-            block.to_neuron()
+            block.to_neuron(n_positions_list)
         self.ln_lm_head.to_neuron()
+        self.program.setup(self.transformer.h, self.ln_lm_head)
+        self.init_program.setup(self.transformer.h, self.ln_lm_head)
 
     def reset(self):
         for block in self.transformer.h:
             block.reset()
-        self.gpt2_caches = []
-        for block in self.transformer.h:
-            self.gpt2_caches.append(block.key_cache)
-            self.gpt2_caches.append(block.value_cache)
-        self.gpt2_params = []
-        for block in self.transformer.h:
-            block_params = [
-                block.ln_1_weight,
-                block.ln_1_bias,
-                block.attn_q_weight,
-                block.attn_q_bias,
-                block.attn_k_weight,
-                block.attn_k_bias,
-                block.attn_v_weight,
-                block.attn_v_bias,
-                block.attn_out_weight,
-                block.attn_out_bias,
-                block.ln_2_weight,
-                block.ln_2_bias,
-                block.mlp_in_weight,
-                block.mlp_in_bias,
-                block.mlp_out_weight,
-                block.mlp_out_bias,
-            ]
-            self.gpt2_params.extend(block_params)
-        ln_lm_head = self.ln_lm_head
-        ln_lm_head_params = [
-            ln_lm_head.ln_f_weight,
-            ln_lm_head.ln_f_bias,
-            ln_lm_head.lm_head_weight,
-        ]
-        self.gpt2_params.extend(ln_lm_head_params)
 
     def forward(self, input_ids, cache_offset, mask):
-        inputs_embeds = self.transformer.wte(input_ids)
-        past_length = cache_offset.item()
+        last_offset = cache_offset[-1].item()
+        bucket_id = find_first_ge_index(self.n_positions_list, last_offset)
         this_length = input_ids.shape[-1]
-        position_ids = torch.arange(past_length, past_length + this_length)
-        position_ids = position_ids.unsqueeze(0).view(-1, this_length)
-        position_embeds = self.transformer.wpe(position_ids)
-        hidden = inputs_embeds + position_embeds
-        hidden = hidden.transpose(0, -1)
-        dtype = dtypes.to_torch_dtype(self.config.amp)
-        hidden = hidden.to(dtype)
-        duplicate = self.manipulator.duplicate
-        hidden = duplicate(hidden)
-        cache_offset = duplicate(cache_offset)
-        mask = duplicate(mask)
-        if self.gpt2_kernel is None:
-            for block in self.transformer.h:
-                hidden = block(hidden, cache_offset, mask)
-            logits = self.ln_lm_head(hidden)
-        else:
-            inputs_cores = hidden, cache_offset, mask, *self.gpt2_caches, *self.gpt2_params
-            logits, *_ = self.gpt2_kernel(inputs_cores)
+        init_length = self.init_program.init_length(this_length, self.init_n_active_tokens)
+        init_step = self.init_program.init_step(self.init_n_active_tokens)
+        for cur_len in range(0, init_length, init_step):
+            slicing = slice(cur_len, cur_len + init_step)
+            inputs = input_ids[:, slicing], cache_offset[slicing], mask[slicing]
+            logits = self._run_program(self.init_program, bucket_id, *inputs)
+        for cur_len in range(init_length, this_length):
+            slicing = slice(cur_len, cur_len + 1)
+            inputs = input_ids[:, slicing], cache_offset[slicing], mask[slicing]
+            logits = self._run_program(self.program, bucket_id, *inputs)
         logits = self.manipulator.unshard_along(logits, dim=0)
         logits = logits.to(torch.float32)
         logits = logits[:self.config.vocab_size]
         logits = logits.transpose(0, -1)
         logits = logits[:, -1, :]
         return logits
+
+    def _run_program(self, program, bucket_id, input_ids, cache_offset, mask):
+        active_n_positions = self.n_positions_list[bucket_id]
+        mask = mask[:, :active_n_positions].contiguous()
+        inputs_embeds = self.transformer.wte(input_ids)
+        past_length = cache_offset[0].item()
+        this_length = input_ids.shape[-1]
+        position_ids = torch.arange(past_length, past_length + this_length)
+        position_ids = position_ids.unsqueeze(0).view(-1, this_length)
+        position_embeds = self.transformer.wpe(position_ids)
+        hidden = inputs_embeds + position_embeds
+        hidden = hidden.transpose(0, -1).contiguous()
+        input_buffers = program.buffers.get_input_buffers(bucket_id)
+        hidden_buffer, *_ = input_buffers
+        hidden = hidden.to(hidden_buffer.dtype)
+        hidden = self.manipulator.duplicate_on_cpu(hidden)
+        cache_offset = self.manipulator.duplicate_on_cpu(cache_offset)
+        mask = self.manipulator.duplicate_on_cpu(mask)
+        for in_buffer, in_tensor in zip(input_buffers, [hidden, cache_offset, mask]):
+            ops.parallel_write(in_buffer, in_tensor)
+        return program.run(bucket_id)
 
     @torch.no_grad()
     def sample(self, input_ids, sequence_length):
@@ -162,29 +151,59 @@ class GPT2ForSampling(module.PretrainedModel):
                 break
         return torch.cat(tokens, dim=-1)
 
+    def register_to_neuron_hook(self, hook):
+        self.to_neuron_hooks.append(hook)
+
+
+class GPT2Buffers:
+
+    def __init__(self, opt_hlo_modules, tp_degree):
+        first_hlo_module, *_ = opt_hlo_modules
+        hidden_buffer = compiler.gen_zero_input(first_hlo_module, 0)
+        cache_offset_buffer = compiler.gen_zero_input(first_hlo_module, 1)
+        mask_buffers = [compiler.gen_zero_input(hlo, 2) for hlo in opt_hlo_modules]
+        self.input_buffers = [hidden_buffer, cache_offset_buffer, mask_buffers]
+        self.output_buffer = compiler.gen_zero_output(first_hlo_module, 0)
+        self.manipulator = parallel.ParallelTensorManipulator(tp_degree)
+
+    def to_neuron(self):
+        hidden_buffer, cache_offset_buffer, mask_buffers = self.input_buffers
+        hidden_buffer = self.manipulator.duplicate(hidden_buffer)
+        cache_offset_buffer = self.manipulator.duplicate(cache_offset_buffer)
+        mask_buffers = [self.manipulator.duplicate(mask_buffer) for mask_buffer in mask_buffers]
+        self.input_buffers = [hidden_buffer, cache_offset_buffer, mask_buffers]
+        self.output_buffer = self.manipulator.duplicate(self.output_buffer)
+
+    def get_input_buffers(self, bucket_id):
+        hidden_buffer, cache_offset_buffer, mask_buffers = self.input_buffers
+        return [hidden_buffer, cache_offset_buffer, mask_buffers[bucket_id]]
+
+
+def find_first_ge_index(values, target):
+    return next(idx for idx, val in enumerate(values) if val >= target)
+
 
 class GPT2Transformer(module.LowMemoryModule):
 
-    def __init__(self, config, block_kernel):
+    def __init__(self, config):
         super().__init__()
         self.wte = module.LowMemoryEmbedding(config.vocab_size, config.n_embd)
         self.wpe = module.LowMemoryEmbedding(config.n_ctx, config.n_embd)
         self.h = module.LowMemoryModuleList()
         for _ in range(config.n_layer):
-            self.h.append(GPT2Block(config, block_kernel))
+            self.h.append(GPT2Block(config))
         self.ln_f = module.LowMemoryLayerNorm(config.n_embd)
 
 
 class GPT2Block(module.LowMemoryModule):
 
-    def __init__(self, config, kernel):
+    def __init__(self, config):
         super().__init__()
         self.ln_1 = module.LowMemoryLayerNorm(config.n_embd)
         self.attn = GPT2Attention(config)
         self.ln_2 = module.LowMemoryLayerNorm(config.n_embd)
         self.mlp = GPT2MLP(config)
         self.config = config
-        self.kernel = kernel
         self.ln_1_weight = None
         self.ln_1_bias = None
         self.attn_q_weight = None
@@ -198,13 +217,13 @@ class GPT2Block(module.LowMemoryModule):
         self.mlp_out_weight = None
         self.mlp_out_bias = None
         self.key_cache = None
+        self.key_cache_slices = None
         self.value_cache = None
-        self.params = None
+        self.value_cache_slices = None
 
-    def to_neuron(self):
-        if self.kernel is not None:
-            self.kernel.load()
-        manipulator = parallel.TensorManipulator(self.config.tp_degree)
+    def to_neuron(self, n_positions_list):
+        config = self.config
+        manipulator = parallel.ParallelTensorManipulator(self.config.tp_degree)
         duplicate = manipulator.duplicate
         shard_along = manipulator.shard_along
         primary_only = manipulator.primary_only
@@ -242,30 +261,52 @@ class GPT2Block(module.LowMemoryModule):
         c_fc.weight = UninitializedParameter()
         c_proj.weight = UninitializedParameter()
 
-    def reset(self):
-        config = self.config
-        manipulator = parallel.TensorManipulator(config.tp_degree)
+        slice_on_nc = manipulator.slice_on_nc
         n_positions = config.n_positions
         batch_size = config.batch_size
         n_head = config.n_head
         s_head = config.n_embd // n_head
         cache_shape = [n_positions, batch_size, n_head, s_head]
         dtype = dtypes.to_torch_dtype(config.amp)
-        self.key_cache = torch.zeros(cache_shape, dtype=dtype)
-        self.key_cache = manipulator.shard_along(self.key_cache, dim=2)
-        self.value_cache = torch.zeros(cache_shape, dtype=dtype)
-        self.value_cache = manipulator.shard_along(self.value_cache, dim=2)
-        self.params = [
-            self.ln_1_weight, self.ln_1_bias, self.attn_q_weight, self.attn_q_bias,
-            self.attn_k_weight, self.attn_k_bias, self.attn_v_weight, self.attn_v_bias,
-            self.attn_out_weight, self.attn_out_bias, self.ln_2_weight, self.ln_2_bias,
-            self.mlp_in_weight, self.mlp_in_bias, self.mlp_out_weight, self.mlp_out_bias,
+        key_cache = torch.zeros(cache_shape, dtype=dtype)
+        key_cache = manipulator.shard_along(key_cache, dim=2)
+        self.key_cache = key_cache
+        self.key_cache_slices = [slice_on_nc(key_cache, 0, start=0, end=npos, step=1)
+                                 for npos in n_positions_list]
+        value_cache = torch.zeros(cache_shape, dtype=dtype)
+        value_cache = manipulator.shard_along(value_cache, dim=2)
+        self.value_cache = value_cache
+        self.value_cache_slices = [slice_on_nc(value_cache, 0, start=0, end=npos, step=1)
+                                   for npos in n_positions_list]
+
+    def get_parameters(self):
+        return [
+            self.ln_1_weight,
+            self.ln_1_bias,
+            self.attn_q_weight,
+            self.attn_q_bias,
+            self.attn_k_weight,
+            self.attn_k_bias,
+            self.attn_v_weight,
+            self.attn_v_bias,
+            self.attn_out_weight,
+            self.attn_out_bias,
+            self.ln_2_weight,
+            self.ln_2_bias,
+            self.mlp_in_weight,
+            self.mlp_in_bias,
+            self.mlp_out_weight,
+            self.mlp_out_bias,
         ]
 
-    def forward(self, hidden, cache_offset, mask):
-        inputs_cores = [hidden, cache_offset, mask, self.key_cache, self.value_cache, *self.params]
-        hidden, self.key_cache, self.value_cache = self.kernel(inputs_cores)
-        return hidden
+    def get_cache_slices(self):
+        return [self.key_cache_slices, self.value_cache_slices]
+
+    def reset(self):
+        zero_cache = torch.zeros(self.key_cache.shape, dtype=self.key_cache.dtype)
+        zero_cache = [zero_cache for _ in range(self.config.tp_degree)]
+        ops.parallel_write(self.key_cache, zero_cache)
+        ops.parallel_write(self.value_cache, zero_cache)
 
 
 class GPT2Attention(module.LowMemoryModule):
@@ -289,41 +330,40 @@ class GPT2MLP(module.LowMemoryModule):
 
 class GPT2LnLmHead:
 
-    def __init__(self, config, kernel, init_kernel, ln_f, lm_head):
-        self.kernel = kernel
-        self.init_kernel = init_kernel
+    def __init__(self, config, ln_f, lm_head):
         self.ln_f = ln_f
         self.lm_head = lm_head
         self.ln_f_weight = None
         self.ln_f_bias = None
         self.lm_head_weight = None
         tp_degree = config.tp_degree
-        self.manipulator = parallel.TensorManipulator(tp_degree)
+        self.manipulator = parallel.ParallelTensorManipulator(tp_degree)
         vocab_size = config.vocab_size
-        self.vocab_pad = ((vocab_size // tp_degree + 1) * tp_degree - vocab_size) % tp_degree
+        self.vocab_pad = utils.pad_vocab_size(vocab_size, tp_degree)
 
     def to_neuron(self):
-        if self.kernel is not None:
-            self.kernel.load()
-        if self.init_kernel is not None:
-            self.init_kernel.load()
-        duplicate = self.manipulator.duplicate
-        shard_along = self.manipulator.shard_along
         self.ln_f.materialize()
-        self.ln_f_weight = duplicate(self.ln_f.weight.detach())
-        self.ln_f_bias = duplicate(self.ln_f.bias.detach())
+        self.ln_f_weight = self.manipulator.duplicate(self.ln_f.weight.detach())
+        self.ln_f_bias = self.manipulator.duplicate(self.ln_f.bias.detach())
         self.lm_head.materialize()
         lm_head_weight = self.lm_head.weight.detach()
         lm_head_weight = torch.nn.functional.pad(lm_head_weight, (0, 0, 0, self.vocab_pad))
-        self.lm_head_weight = shard_along(lm_head_weight.T, dim=1)
+        self.lm_head_weight = self.manipulator.shard_along(lm_head_weight.T, dim=1)
         self.lm_head.weight = UninitializedParameter()
 
-    def __call__(self, hidden):
-        inputs = hidden, self.ln_f_weight, self.ln_f_bias, self.lm_head_weight
-        logits, = self.kernel(inputs)
-        return logits
+    def get_parameters(self):
+        return [self.ln_f_weight, self.ln_f_bias, self.lm_head_weight]
 
-    def call_init(self, hidden):
-        inputs = hidden, self.ln_f_weight, self.ln_f_bias, self.lm_head_weight
-        logits, = self.init_kernel(inputs)
-        return logits
+
+def build_gpt2_program(config, n_active, n_positions_list, n_layers):
+    hlo_modules = [hlo.build_gpt2_hlo_module(config, n_active, npos) for npos in n_positions_list]
+    buffers = GPT2Buffers(hlo_modules, config.tp_degree)
+    if n_layers == config.n_layer:
+        return program.FullyUnrolledDecoder(config.tp_degree, hlo_modules, buffers)
+    else:
+        build_func = hlo.build_gpt2_multi_block_hlo_module
+        hlo_modules = [build_func(config, n_active, npos, n_layers) for npos in n_positions_list]
+        head_hlo_module = hlo.build_ln_lm_head_hlo_module(config, n_active)
+        return program.MultiLayerDecoder(config.n_layer, config.tp_degree, hlo_modules,
+                                         head_hlo_module, n_layers, buffers)
+
