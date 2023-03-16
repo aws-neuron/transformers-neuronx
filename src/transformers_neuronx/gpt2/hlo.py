90f8a0e0997cc6feef4a1f5f6dffd4cea454e198
Haichen Li <lhaiche@amazon.com>
2023-03-16 21:00:02 +0000

    [gpt2] fix one more mentioning of decoder_attention_mask
    SIM: https://i.amazon.com/kaena-15629
    cr: https://code.amazon.com/reviews/CR-87487660

--- src/transformers_neuronx/gpt2/hlo.py
+++ src/transformers_neuronx/gpt2/hlo.py
@@ -312,7 +312,7 @@ def gen_scribable_gpt2(config, n_active_tokens, n_positions, blocks_u8_bounds=No
         blocks_caches = [gen_block_caches() for _ in range(n_layer)]
         blocks_params = [gen_block_params() for _ in range(n_layer)]
         ln_lm_head_params = gen_ln_lm_head_params()
-        mask = hlo.decoder_attention_mask(cache_offset, scribe.f32, n_positions)
+        mask = hlo.decoder_attention_mask_legacy(cache_offset, scribe.f32, n_positions)
         return gpt2(hidden, cache_offset, mask, blocks_caches, blocks_params, ln_lm_head_params,
                     dequant_dtype, blocks_u8_bounds, config)
 
