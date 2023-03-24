8930c89689662f981c627e4503b17b9b6b4c94e0
Hesam Ilati <ilati@amazon.com>
2023-03-24 00:11:05 +0000

    GPT2 proxy model generation on the fly
    # This is a combination of 3 commits.
    
    commit: 9e9e4d42c15e59156c18c356ba6a6a6f26c4628c
    Author: Hesam Ilati <ilati@amazon.com>
    Date: 2023-03-24T00:06:35.000Z
    
    Minor upadte to avoid issues if init_std does not exist. https://code.amazon.com/reviews/CR-87906690
    
    commit: 9905eef170535e57de399c52389a4948aa761563
    Author: Hesam Ilati <ilati@amazon.com>
    Date: 2023-03-23T21:53:23.000Z
    
    Fixing the issue at runtime when loading the model.
    
    GitOrigin-RevId: 060641590a967cffb12c14bdcc4b96ff56f7677e

--- src/transformers_neuronx/module.py
+++ src/transformers_neuronx/module.py
@@ -60,7 +60,7 @@ class LowMemoryModule(torch.nn.Module):
                 if param._file_path.endswith('.empty_json'):
                     with open(param._file_path) as fp:
                         empty_json = json.load(fp)
-                    input_param = empty_json['init_std'] * torch.randn(empty_json['shape'])
+                    input_param = empty_json.get('init_std', 1.0) * torch.randn(empty_json['shape'])
                     dtype = getattr(torch, empty_json['torch_dtype'])
                     input_param = input_param.to(dtype)
                 else:
