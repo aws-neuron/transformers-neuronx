060641590a967cffb12c14bdcc4b96ff56f7677e
Hesam Ilati <ilati@amazon.com>
2023-03-24 00:11:05 +0000

    GPT2 proxy model generation on the fly
    # This is a combination of 3 commits.
    
    commit: 9e9e4d42c15e59156c18c356ba6a6a6f26c4628c
    Author: Hesam Ilati <ilati@amazon.com>
    Date: 2023-03-24T00:06:35.000Z
    
    Minor upadte to avoid issues if init_std does not exist. https://code.amazon.com/reviews/CR-87906690
    
    commit: 9905eef170535e57de399c52389a4948aa761563
    Author: Hesam Ilati <ilati@amazon.com>
    Date: 2023-03-23T21:53:23.000Z
    
    Fixing the issue at runtime when loading the model.
    
    CR: https://code.amazon.com/reviews/CR-87906690
    SIM: https://sim.amazon.com/issues/kaena-15850
    
    commit: ff97741aa95b3f5f316a6023d0111e9fe17153f8
    Author: Hesam Ilati <ilati@amazon.com>
    Date: 2023-03-23T02:42:00.000Z
    
    Now this generates GPT2 proxy models on the fly with same folder names and order as the pretrained one. But still runtime issue exists.  https://sim.amazon.com/issues/kaena-15850
    
    cr: https://code.amazon.com/reviews/CR-87906690

--- src/transformers_neuronx/gpt2/gen_random_pretrained.py
+++ src/transformers_neuronx/gpt2/gen_random_pretrained.py
@@ -21,42 +21,52 @@ from transformers.models.gpt2 import GPT2Config
 from transformers_neuronx.module import sanitize_file_name, _KEY_TO_FILENAME_JSON
 from transformers.configuration_utils import PretrainedConfig
 
-def gen_random_pretrained(model_name, save, empty=False):
-    config = GPT2Config.from_pretrained(model_name).to_dict()
+def gen_random_pretrained(model_name, save, empty=False, print_shapes=False):
+    if '.json' in model_name:
+        config = json.load(open(model_name))
+    else:
+        config = GPT2Config.from_pretrained(model_name).to_dict()
     os.makedirs(save, exist_ok=True)
     with open(os.path.join(save, 'config.json'), 'w') as fp:
         json.dump(config, fp, indent=2)
     vocab_size = config['vocab_size']
     hidden_size = config['n_embd']
     max_position_embeddings = config['n_ctx']
-    ffn_dim = hidden_size * 4
+    ffn_dim = hidden_size #TODO: Check this ffn_dim = hidden_size or ffn_dim = hidden_size * 4 (this fails)
     num_hidden_layers = config['n_layer']
-    torch_dtype = config.get('torch_dtype', 'bf16')
+    torch_dtype = config.get('torch_dtype', None)
+    if torch_dtype is None:
+        torch_dtype = 'float16'
     init_std = 0.001
     name2shape = {
         'transformer.wte.weight': [vocab_size, hidden_size],
         'transformer.wpe.weight': [max_position_embeddings, hidden_size],
-        'transformer.ln_f.weight': [hidden_size],
-        'transformer.ln_f.bias': [hidden_size],
     }
     layer_name2shape = {
+        'ln_1.weight': [hidden_size],
+        'ln_1.bias': [hidden_size],      
+        'attn.bias': [hidden_size],
+        'attn.masked_bias': [hidden_size],
         'attn.c_attn.weight': [hidden_size, 3 * hidden_size],
         'attn.c_attn.bias': [3 * hidden_size],
         'attn.c_proj.weight': [hidden_size, hidden_size],
         'attn.c_proj.bias': [hidden_size],
-        'ln_1.weight': [hidden_size],
-        'ln_1.bias': [hidden_size],
+        'ln_2.weight': [hidden_size],
+        'ln_2.bias': [hidden_size],  
         'mlp.c_fc.weight': [ffn_dim, hidden_size], 
         'mlp.c_fc.bias': [ffn_dim],
         'mlp.c_proj.weight': [hidden_size, ffn_dim],
         'mlp.c_proj.bias': [hidden_size],
-        'ln_f.weight': [hidden_size],
-        'ln_f.bias': [hidden_size],
     }
     for idx in range(num_hidden_layers):
         for name, shape in layer_name2shape.items():
             name2shape[f'transformer.h.{idx}.{name}'] = shape
+    name2shape['transformer.ln_f.weight'] = [hidden_size]
+    name2shape['transformer.ln_f.bias'] = [hidden_size]
     name2shape['lm_head.weight'] = [vocab_size, hidden_size]
+    if print_shapes:
+        print("Components' shapes")
+        [print(x) for x in name2shape.items()]
     key_to_filename = {}
     for idx, key in enumerate(name2shape.keys()):
         key_to_filename[key] = f'p{idx}.{sanitize_file_name(key)}'
@@ -66,7 +76,7 @@ def gen_random_pretrained(model_name, save, empty=False):
     os.makedirs(split_param_dir, exist_ok=True)
     with open(os.path.join(split_param_dir, _KEY_TO_FILENAME_JSON), 'w') as fp:
         json.dump(key_to_filename, fp, indent=2)
-    dtype = torch_dtype #getattr(torch, torch_dtype)
+    dtype = getattr(torch, torch_dtype)
     for name, shape in name2shape.items():
         save_path = os.path.join(split_param_dir, key_to_filename[name])
         factor = 0.0 if 'layer_norm' in name or 'bias' in name else init_std
@@ -82,6 +92,3 @@ def gen_random_pretrained(model_name, save, empty=False):
         init_param = init_param.to(dtype)
         torch.save(init_param, save_path)
         print(f'done saving {save_path}')
-
-if __name__ == "__main__":
-    gen_random_pretrained("gpt2", "/home/ubuntu/hesam/02_06_2023___20_06_59/proj9_gpt/random_on_fly/rand-split")
