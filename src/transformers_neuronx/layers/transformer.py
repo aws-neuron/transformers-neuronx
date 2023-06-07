6352f0ae9644d5d5603d024a730f88c171e1cbca
Jonathan Lunt <jlunt@amazon.com>
2023-06-07 20:50:59 +0000

    Added rms_lm_head for LLaMa-like models
    GitOrigin-RevId: a99d88ab614cdf6bcaf1d4a26034595391986d21

--- src/transformers_neuronx/layers/transformer.py
+++ src/transformers_neuronx/layers/transformer.py
@@ -17,12 +17,15 @@ from transformers_neuronx import hlo
 
 def ln_lm_head(hidden, ln_f_weight, ln_f_bias, lm_head_weight, lm_head_bias):
     """
-    Language model head with normalization. This slices the hidden input to
-    compute  output for a single output token rather than `n_active_tokens`.
-    During context encoding this means that the next token logits will be
-    computed *only* for the last context token.
+    Language model head with layer normalization.
 
-    logits = (norm(H) @ W) + B
+    This slices the hidden input to compute output for a single output token
+    rather than `n_active_tokens`. During context encoding this means that
+    the next token logits will be computed *only* for the last context token.
+
+    Models: GPT2, OPT, GPT-J, GPTNeoX, BLOOM.
+
+    logits = (layer_norm(H) @ W) + B
     """
     hidden_size, n_active_tokens, batch_size = hidden.sizes
     dtype = hidden.dtype
@@ -38,6 +41,40 @@ def ln_lm_head(hidden, ln_f_weight, ln_f_bias, lm_head_weight, lm_head_bias):
     ln_hidden = hlo.layer_norm(hidden, ln_f_weight, ln_f_bias)
     ln_hidden = dtype[hidden_size,n_active_tokens*batch_size].Reshape(ln_hidden)
     logits = hlo.dot00(lm_head_weight, ln_hidden)
+    if lm_head_bias is not None:
+        lm_head_bias = dtype[logits.sizes].Broadcast(lm_head_bias, dimensions=[0])
+        logits = dtype[logits.sizes].Add(logits, lm_head_bias)
+    vocab_size, _ = logits.sizes
+    result = dtype[vocab_size,n_active_tokens,batch_size].Reshape(logits)
+    return result
+
+
+def rms_lm_head(hidden, rms_weight, lm_head_weight, lm_head_bias, eps=1e-6):
+    """
+    Language model head with rms normalization.
+
+    This slices the hidden input to compute output for a single output token
+    rather than `n_active_tokens`. During context encoding this means that
+    the next token logits will be computed *only* for the last context token.
+
+    Models: LLaMa.
+
+    logits = (rms_norm(H) @ W) + B
+    """
+    hidden_size, n_active_tokens, batch_size = hidden.sizes
+    dtype = hidden.dtype
+    if n_active_tokens > 1:
+        slice_dimensions = [
+            dict(start=0, limit=hidden_size, stride=1),
+            dict(start=n_active_tokens - 1, limit=n_active_tokens, stride=1),
+            dict(start=0, limit=batch_size, stride=1),
+        ]
+        n_active_tokens = 1
+        sizes = hidden_size, n_active_tokens, batch_size
+        hidden = dtype[sizes].Slice(hidden, slice_dimensions=slice_dimensions)
+    rms_hidden = hlo.rms_norm(hidden, rms_weight, eps)
+    rms_hidden = dtype[hidden_size, n_active_tokens * batch_size].Reshape(rms_hidden)
+    logits = hlo.dot00(lm_head_weight, rms_hidden)
     if lm_head_bias is not None:
         lm_head_bias = dtype[logits.sizes].Broadcast(lm_head_bias, dimensions=[0])
         logits = dtype[logits.sizes].Add(logits, lm_head_bias)
