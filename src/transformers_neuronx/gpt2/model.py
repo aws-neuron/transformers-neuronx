373e40def837ac647b54ef582713150ed5fc2860
Bowen Chen <bowencc@amazon.com>
2023-07-09 23:22:14 +0000

    remove comment for beam search in GPT2ForHuggingFaceSampling
    GitOrigin-RevId: 71dc8d59617e27e2b8e3517edc6064c18c9c09c3

--- src/transformers_neuronx/gpt2/model.py
+++ src/transformers_neuronx/gpt2/model.py
@@ -290,20 +290,6 @@ class GPT2ForHuggingFaceSampling(module.PretrainedModel, PreTrainedModel):
         if  output_hidden_states or output_attentions or attention_mask is not None:
             warnings.warn("Warning: These arguments are not used by forward(): \
                 (output_hidden_states, output_attentions, attention_mask)")
-        # TODO (bowencc): Need to further verify the behavior of attention_mask and position_ids under beam search, comment out for now
-        # batch_dim = input_ids.shape[0]
-        # batch_size = self.config.batch_size
-        # if batch_dim != batch_size:
-        #     if batch_dim < batch_size or batch_dim % batch_size != 0:
-        #         raise ValueError(f"batch dimension on input_ids ({batch_dim}) is not compatible with model's compiled batch_size ({batch_size})")
-        #     input_ids_splits = input_ids.reshape(batch_size, batch_dim//batch_size, -1).transpose(1, 0).split(1, dim=0)
-        #     out_logits = []
-        #     # iterate per beam
-        #     for split in input_ids_splits:
-        #         out = self._forward(split.squeeze(0), cache_ids, start_ids)
-        #         out_logits.append(out)
-        #     out_logits = torch.cat(out_logits, dim=1).reshape(batch_dim, 1, -1)
-        # else:
         out_logits = self._forward(input_ids, cache_ids, start_ids)
         if return_dict:
             return ModelOutput(
