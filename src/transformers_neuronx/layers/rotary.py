15a36c8ee38c536d801c496a66536561cf106bc2
Shashwat Srijan <sssrijan@amazon.com>
2024-07-23 18:32:26 +0000

    Rope scaling inline with transformers impl
    GitOrigin-RevId: 63bed4934ebe31df4ad606b2dbfd65e453e2edce

--- src/transformers_neuronx/layers/rotary.py
+++ src/transformers_neuronx/layers/rotary.py
@@ -17,29 +17,23 @@ import math
 from transformers_neuronx import hlo
 
 
-def apply_inv_frequency_scaling(freqs, rope_scaling):
-    # Values obtained from grid search
-    scale_factor = rope_scaling.get('scale_factor')
+def apply_inv_frequency_scaling(freq, rope_scaling):
+    scale_factor = rope_scaling.get('factor')
     low_freq_factor = rope_scaling.get('low_freq_factor')
     high_freq_factor = rope_scaling.get('high_freq_factor')
-    old_context_len = rope_scaling.get('old_context_len')
+    old_context_len = rope_scaling.get('original_max_position_embeddings')
 
     low_freq_wavelen = old_context_len / low_freq_factor
     high_freq_wavelen = old_context_len / high_freq_factor
-    new_freqs = []
-    for freq in freqs:
-        wavelen = 2 * math.pi / freq
-        if wavelen < high_freq_wavelen:
-            new_freqs.append(freq)
-        elif wavelen > low_freq_wavelen:
-            new_freqs.append(freq / scale_factor)
-        else:
-            assert low_freq_wavelen != high_freq_wavelen
-            smooth = (old_context_len / wavelen - low_freq_factor) / (
-                high_freq_factor - low_freq_factor
-            )
-            new_freqs.append((1 - smooth) * freq / scale_factor + smooth * freq)
-    return torch.tensor(new_freqs, dtype=freqs.dtype)
+    assert low_freq_wavelen != high_freq_wavelen
+
+    wavelen = 2 * math.pi / freq
+    smooth = (old_context_len / wavelen - low_freq_factor) / (high_freq_factor - low_freq_factor)
+
+    new_freq = torch.where(wavelen < high_freq_wavelen, freq, freq/scale_factor)
+    smooth_cond = torch.logical_and(wavelen >= high_freq_wavelen, wavelen <= low_freq_wavelen)
+    new_freq = torch.where(smooth_cond, (1 - smooth) * freq / scale_factor + smooth * freq, new_freq)
+    return new_freq.to(dtype=freq.dtype)
 
 
 def rotary_embedding(head_dim, cache_ids, base=10000, interpolation_factor=None):
@@ -75,7 +69,7 @@ def hlo_rotary_embedding(dtype, head_dim, cache_ids, base=10000, interpolation_f
     size = head_dim // 2
 
     inv_freq = 1.0 / (base ** (torch.arange(0, head_dim, 2) / head_dim))
-    if rope_scaling is not None:
+    if rope_scaling is not None and rope_scaling.get("rope_type", rope_scaling.get("type", None)) == "llama3":
         inv_freq = apply_inv_frequency_scaling(inv_freq, rope_scaling)
     inv_freq = hlo.literal(mtype, inv_freq)
 
