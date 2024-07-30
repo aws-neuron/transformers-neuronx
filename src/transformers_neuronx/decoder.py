056ff4c569b91e9bd4db80cc67f415104493af17
Udit Deshmukh <desudit@amazon.com>
2024-07-30 14:07:24 -0700

    Fix KV head full replication logic
    GitOrigin-RevId: 1e833d09cef6051223fc88e9ef83b960e43a754a

--- src/transformers_neuronx/decoder.py
+++ src/transformers_neuronx/decoder.py
@@ -1353,9 +1353,13 @@ class DecoderLayer(torch.nn.Module):
 
             if n_kv_heads_padded != self.n_kv_head:
 
-                if n_kv_heads_padded % self.n_kv_head == 0 and extra_heads == 0:
+                if n_kv_heads_padded % self.n_kv_head == 0:
                     ratio = int(n_kv_heads_padded / self.n_kv_head)
                 else:
+                    ratio = int((n_kv_heads_padded - extra_heads) / self.n_kv_head)
+
+                # Full replication: replicate KV heads to original Q heads and then do padding
+                if n_head_padded == n_kv_heads_padded and extra_heads > 0:
                     ratio = int((n_kv_heads_padded - extra_heads) / self.n_kv_head)
 
                 def repeat(weight):
